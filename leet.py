b6bad21034d5658a58c60f12f755ecf67a604903
Cal <72684566+Jimdangle@users.noreply.github.com>
2021-02-14 19:06:56 -0800

    Changed some things
    news.py:
       - added 5th parameter  called debug
          default it is set to true
          when debug is true the console will display when connections are made and to where, along with the corresponding status code
    
     leet.py
        - created a function called findStonk() with a parameter for a stock. This function is only used by the symbolHandler function

--- leet.py
+++ leet.py
@@ -1,42 +1,88 @@
-import requests
-from bs4 import BeautifulSoup
 from news import newsScraper
 import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+
+#FUNCTIONS
+# stronk base info
+def findStonk(stonk):
+    yahoo = 'http://finance.yahoo.com/quote/'
+    str = yahoo + stonk.upper() + "?p=" + stonk.upper() + "&.tsrc=fin-srch"
+
+    stonk_request = requests.get(str)
+    stonk_soup = BeautifulSoup(stonk_request.content, 'html.parser')
+
+    cur_price = stonk_soup.find('span', attrs={'data-reactid': '50'}).string
+    cur_change = stonk_soup.find('span', attrs={'data-reactid': '51'}).string
+    today_volume = stonk_soup.find('span', attrs={'data-reactid': '126'}).string
+
+    return [stonk,cur_price, cur_change, today_volume]
+
+
+# Symbol handler
+
+def symbolHandler(symbol_list):
+    return_li = []
+    for symbol in symbol_list:
+        return_li.append(findStonk(symbol))
+
+    return return_li
 
 #list of news scraper class objects
 newsScrappers = []
+#list of all found symbols
+symbolList = []
+cleanSymbols = []
+#set to true if you want the program to generate graphs for each site
+graph = True
 
-
-
-yf = newsScraper('https://finance.yahoo.com/','news',10,"yahoo")
+# added new parameter to newsScraper
+# the final argurment is debug. Default it is true
+# if debug = True it will print out if the connection was successful or not
+yf = newsScraper('https://finance.yahoo.com/','news',10,"yahoo",False)
 print(yf.word_count)
 newsScrappers.append(yf)
 
-#this is misnamed but i didnt want it to make another graph so
-usatoday = newsScraper('https://www.cnbc.com/economy/','2021',10,'usa')
-print(usatoday.word_count)
-newsScrappers.append(usatoday)
+cnbc = newsScraper('https://www.cnbc.com/economy/','2021',10,'cnbc',False)
+print(cnbc.word_count)
+newsScrappers.append(yf)
 
-# this one aint working
-#reut = newsScraper('https://www.reuters.com/business','article',10,'reut')
-#print(reut.word_count)
-#newsScrappers.append(reut)
+yf_dict = yf.word_count
+cnbc_dict = cnbc.word_count
 
-
-#just to turn of graphing
-graph = True
-
-if graph is True:
-    for newsBoi in newsScrappers:
-        if newsBoi.p_data is not None:
-            count_dict = newsBoi.word_count
-            keys = count_dict.keys()
-            values = count_dict.values()
-            plt.figure(figsize=(len(values),10))
-            scat = plt.bar(keys,values)
-            plt.savefig(newsBoi.nickname+".png")
+#take the newscrappers word counts and get all of their symbols into a larger list
+for scraper in newsScrappers:
+    #pull word count di
+    count_items = scraper.word_count.items()
+    new_count = {str(key) : str(values) for key, values in count_items}
+    keys = list(new_count.keys())
+    for key in keys:
+        symbolList.append(key)
+#remove duplicate entries from symbolList and turns into cleanSymbols
+for symbol in symbolList:
+    if symbol not in cleanSymbols:
+        cleanSymbols.append(symbol)
 
 
+#get stonk info
+print(symbolList)
+print(symbolHandler(cleanSymbols))
 
-def combineData():
-    print('asd')
+#graphing (not working atm)
+if graph is False:
+    for scraper in newsScrappers:
+        if scraper.p_data is not None:
+            current_count = scraper.word_count
+            keys = current_count.keys
+            print(keys)
+            values = current_count.values
+            plt.figure(figsize=(len(keys), 10))
+            scat = plt.bar(keys, values)
+            plt.savefig(scraper.nickname + ".png")
+
+
+
+
+
+
+
--- news.py
+++ news.py
@@ -1,14 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-from datetime import datetime
-import time
 import random
-import yfinance as yf
+
 
 class newsScraper:
 
-    def __init__(self, url, sep, tol=10,nickname="example"):
+    def __init__(self, url, sep, tol=10,nickname="example",debug=True):
         self.headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'referer' : 'https://www.google.com/',
@@ -17,6 +15,7 @@ class newsScraper:
         self.sep = sep
         self.tol = tol
         self.nickname = nickname
+        self.debug = debug
 
         # list for filtered links
         self.news_links = []
@@ -31,7 +30,8 @@ class newsScraper:
         main_request = main_session.get(url,headers=self.headers)
         if main_request.status_code == 200:
             main_soup = BeautifulSoup(main_request.text, 'html.parser')
-            print(main_request.headers)
+            if self.debug is True:
+                print(main_request.headers)
             # main links
             # get <a> elements from main content
             main_links = main_soup.find_all('a')
@@ -49,7 +49,8 @@ class newsScraper:
                         if href.find(self.sep) >= self.tol and href.find(self.sep) != -1:
                             self.news_links.append(href)
         else:
-            print("connection failed STATUS CODE : " + str(main_request.status_code))
+            if self.debug is True:
+                print("connection failed STATUS CODE : " + str(main_request.status_code))
             main_session.close()
 
         for news in self.news_links:
@@ -61,9 +62,11 @@ class newsScraper:
             #wait_time = random.randrange(2,15)
             #time.sleep(wait_time)
             news_request = main_session.get(news,headers=self.headers)
-            print(news_request)
+            if self.debug is True:
+                print(news_request)
             if news_request.status_code == 200:
-                print('Connected to : ' + news)
+                if self.debug is True:
+                    print('Connected to : ' + news)
                 news_soup = BeautifulSoup(news_request.text,'html.parser')
                 # find all p elements
                 news_ps = news_soup.find_all('p')
@@ -73,7 +76,8 @@ class newsScraper:
                     if news_string is not None:
                         self.p_data += news_string
             else:
-                print('not found ' + news + " STATUS CODE: "+ str(news_request.status_code))
+                if self.debug is True:
+                    print('not found ' + news + " STATUS CODE: "+ str(news_request.status_code))
 
 
         main_session.close()
@@ -99,7 +103,10 @@ class newsScraper:
                 self.word_count[data] = self.p_data.count(data)
 
 
-# class for using yfinance with the word count data
+
+
+
+
 
 
 
