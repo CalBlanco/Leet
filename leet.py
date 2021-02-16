ce1a765de07be7637df4d50504403afd91d8bda4
Cal <72684566+Jimdangle@users.noreply.github.com>
2021-02-16 00:39:47 -0800

    Added a bunch of comments to hopefully clear a lot of things up.
    Fixed :
        Graphing - thanks adrien
        stockdata - fuck off yahoo
    
    added :
        A graph that plots symbol and its %change

--- leet.py
+++ leet.py
@@ -4,22 +4,58 @@ import requests
 from bs4 import BeautifulSoup
 
 #FUNCTIONS
-# stronk base info
+
+''' FINDSTONK is my shortcut to using the yfinance api. It uses the requests lib and beautifulsoup.
+    
+    @note : this function is only used by the symbolHandler function currently
+    
+    @param: it takes one argument and that is a stock symbol like AAPL, or something.
+    
+    @return a list with the symbol itself, the current price, and the percentage change
+    
+    str is the url that it is going to grab data from. the concatenation of the strings represent where in the link the.
+    symbol must be placed in order for it to find the right page.
+    
+    after the request is made a BeautifulSoup object is made named stonk_soup.
+    There were some issues in finding info between NYSE listed stocks and other markets so to fix that.
+    It finds a specific <div> element with id=quote-header-info.
+    this div contains the stock pricing info
+    the reason for the negative indexing is that different symbols had different locations for the price.
+    However they were always the second and third to last items.
+    I turn these values into strings to get rid of all the html code in them
+    
+    Then i convert the cur change to the percentage change by locating the numerical value after the first ( and before.
+    the % sign. Cast that value to a float for graphing later.'''
+
 def findStonk(stonk):
     yahoo = 'http://finance.yahoo.com/quote/'
-    str = yahoo + stonk.upper() + "?p=" + stonk.upper() + "&.tsrc=fin-srch"
-
-    stonk_request = requests.get(str)
+    url = yahoo + stonk.upper() + "?p=" + stonk.upper() + "&.tsrc=fin-srch"
+    print(url)
+    stonk_request = requests.get(url)
     stonk_soup = BeautifulSoup(stonk_request.content, 'html.parser')
 
-    cur_price = stonk_soup.find('span', attrs={'data-reactid': '50'}).string
-    cur_change = stonk_soup.find('span', attrs={'data-reactid': '51'}).string
-    today_volume = stonk_soup.find('span', attrs={'data-reactid': '126'}).string
+    stonk_quote = stonk_soup.find('div',{'id':'quote-header-info'})
+    stonk_data = stonk_quote.find_all('span')
 
-    return [stonk,cur_price, cur_change, today_volume]
+    cur_price = stonk_data[-3].string
+    cur_change = stonk_data[-2].string
+    percent_change = cur_change[cur_change.find('(')+1:cur_change.find('%')]
+    percent_change = float(percent_change)
+    return [stonk,cur_price,percent_change]
 
 
 # Symbol handler
+''' SYMBOLHANDLER is a function for taking all of the found symbols and creating a list of values from the STONKFINDER
+    method.
+    
+    @param: list of symbols we want information for
+    
+    @return : list of those symbols info 
+    
+    Super basic function. Just uses stonkFinder() and then appends that information to a list.
+    Decided to split these into two separate functions to increase modularity of stonkFinder(). 
+
+'''
 
 def symbolHandler(symbol_list):
     return_li = []
@@ -36,16 +72,14 @@ cleanSymbols = []
 #set to true if you want the program to generate graphs for each site
 graph = True
 
-# added new parameter to newsScraper
-# the final argurment is debug. Default it is true
-# if debug = True it will print out if the connection was successful or not
+
 yf = newsScraper('https://finance.yahoo.com/','news',10,"yahoo",False)
 print(yf.word_count)
 newsScrappers.append(yf)
 
 cnbc = newsScraper('https://www.cnbc.com/economy/','2021',10,'cnbc',False)
 print(cnbc.word_count)
-newsScrappers.append(yf)
+newsScrappers.append(cnbc)
 
 yf_dict = yf.word_count
 cnbc_dict = cnbc.word_count
@@ -65,23 +99,34 @@ for symbol in symbolList:
 
 
 #get stonk info
-print(symbolList)
-print(symbolHandler(cleanSymbols))
+#print(symbolList)
+handled = symbolHandler(cleanSymbols)
+print(handled)
 
 #graphing (not working atm)
-if graph is False:
+if graph is True:
+    #This portion creates the graphs for frequency of symbols per news site
     for scraper in newsScrappers:
         if scraper.p_data is not None:
             current_count = scraper.word_count
-            keys = current_count.keys
-            print(keys)
-            values = current_count.values
+            keys = current_count.keys()
+            values = current_count.values()
             plt.figure(figsize=(len(keys), 10))
-            scat = plt.bar(keys, values)
+            plt.bar(keys, values)
+
             plt.savefig(scraper.nickname + ".png")
 
 
+    #This portion creates a graph of all the symbols with their respective percentage change
+    company = []
+    percent_change = []
+    for ticker in handled:
+        company.append(ticker[0])
+        percent_change.append(ticker[2])
 
+    plt.figure(figsize=(len(company), 10))
+    plt.bar(company, percent_change)
+    plt.savefig('stronk.png')
 
 
 
--- news.py
+++ news.py
@@ -6,11 +6,35 @@ import random
 
 class newsScraper:
 
+    '''Main Scraping class.
+
+        @params:
+            url for finding main site, sep aka separating term, tol : find separator past certain point
+            nickname for graph file name, debug : true = print connection status, false = no print.
+
+        @important_variables:
+            news_links(all the links that meet the sep and tolerance criteria
+            p_data(the raw paragraph data from each link)
+            word_count(symbols and their frequency in a dictionary) - main var
+
+        @mechanics:
+            this class takes in a main link(url) and finds related news links based on the sep and tol parameters.
+            Individual requests are then made to each child link, with their <p> tags placed in a list called p_data
+            That entire list is then filtered through the StonkData file and counted which then turns into word_count.
+
+            @minor:
+                There are somethings that might not make sense in here. Some websites require certain information
+                or behavior in order to filter out bots and humans. Anything that has to do with timing, or header
+                information is just for that. Do not touch them and ask me if you plan on making changes.
+    '''
+
     def __init__(self, url, sep, tol=10,nickname="example",debug=True):
+        # dont touch these
         self.headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'referer' : 'https://www.google.com/',
-    }
+        }
+
         self.url = url
         self.sep = sep
         self.tol = tol
@@ -26,8 +50,8 @@ class newsScraper:
         print('session started : ' + self.url)
 
         # main request from website, content and then soup
-
         main_request = main_session.get(url,headers=self.headers)
+
         if main_request.status_code == 200:
             main_soup = BeautifulSoup(main_request.text, 'html.parser')
             if self.debug is True:
@@ -35,50 +59,55 @@ class newsScraper:
             # main links
             # get <a> elements from main content
             main_links = main_soup.find_all('a')
+            main_p = main_soup.find_all('p')
             # find the news related links
-            for links in main_links:
-                # get the href attribute from the <a> elements / from main_links
-                href = links.get('href')
-                # make sure there is a value to look at
-                if href is not None:
-                    # make sure it is not a duplicate with another link
-                    if href not in self.news_links:
-                        # if it can not find the base url in the news urls add the base to the news url
-                        # noticed a problem where this actually pasted the url twice so watch out
-                        # could use more work
-                        if href.find(self.sep) >= self.tol and href.find(self.sep) != -1:
-                            self.news_links.append(href)
+            if main_links:
+                for links in main_links:
+                    # get the href attribute from the <a> elements / from main_links
+                    href = links.get('href')
+                    # make sure there is a value to look at
+                    if href is not None:
+                        # make sure it is not a duplicate with another link
+                        if href not in self.news_links:
+                            # if it can not find the base url in the news urls add the base to the news url
+                            # noticed a problem where this actually pasted the url twice so watch out
+                            # could use more work
+                            if href.find(self.sep) >= self.tol and href.find(self.sep) != -1:
+                                self.news_links.append(href)
+            else:
+                if debug is True:
+                    print('no links found')
         else:
             if self.debug is True:
                 print("connection failed STATUS CODE : " + str(main_request.status_code))
             main_session.close()
 
-        for news in self.news_links:
-            main_session.cookies.clear()
-            # news request -> content -> soup : previously known as
+        if self.news_links:
+            for news in self.news_links:
+                main_session.cookies.clear()
+                # news request -> content -> soup : previously known as
 
-            #experimenting with delays on requests so that we don't get flagged
-            wait_seed = random.seed()
-            #wait_time = random.randrange(2,15)
-            #time.sleep(wait_time)
-            news_request = main_session.get(news,headers=self.headers)
-            if self.debug is True:
-                print(news_request)
-            if news_request.status_code == 200:
+                #experimenting with delays on requests so that we don't get flagged
+                wait_seed = random.seed()
+                #wait_time = random.randrange(2,15)
+                #time.sleep(wait_time)
+                news_request = main_session.get(news,headers=self.headers)
                 if self.debug is True:
-                    print('Connected to : ' + news)
-                news_soup = BeautifulSoup(news_request.text,'html.parser')
-                # find all p elements
-                news_ps = news_soup.find_all('p')
-                # convert all paragraphs to a single string
-                for news_p in news_ps:
-                    news_string = news_p.string
-                    if news_string is not None:
-                        self.p_data += news_string
-            else:
-                if self.debug is True:
-                    print('not found ' + news + " STATUS CODE: "+ str(news_request.status_code))
-
+                    print(news_request)
+                if news_request.status_code == 200:
+                    if self.debug is True:
+                        print('Connected to : ' + news)
+                    news_soup = BeautifulSoup(news_request.text,'html.parser')
+                    # find all p elements
+                    news_ps = news_soup.find_all('p')
+                    # convert all paragraphs to a single string
+                    for news_p in news_ps:
+                        news_string = news_p.string
+                        if news_string is not None:
+                            self.p_data += news_string
+                else:
+                    if self.debug is True:
+                        print('not found ' + news + " STATUS CODE: "+ str(news_request.status_code))
 
         main_session.close()
         print('ending session : ' + self.url)
