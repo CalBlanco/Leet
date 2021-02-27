fa35509405e0c1f754b04f99eeb8eb82de76fc65
acarrou <50222631+acarrou@users.noreply.github.com>
2021-02-27 10:18:46 -0800

    Update RedditScrapeEx.py
    new example

--- RedditScrapeEx.py
+++ RedditScrapeEx.py
@@ -1,5 +1,11 @@
 from StonkRedditAPI import StonkRedditAPI
+from YahooFinanceSearch import YahooFinanceSearch
 
+test1 = StonkRedditAPI('r/wallstreetbets', 'hot', 'all', 100)
+test2 = StonkRedditAPI('r/Wallstreetbetsnew', 'hot', 'all', 100)
+test3 = StonkRedditAPI('r/WallStreetbetsELITE', 'hot', 'all', 100)
+print('r/wallstreetbets', test1.word_count)
+print('r/Wallstreetbetsnew', test2.word_count)
+print('r/WallStreetbetsELITE', test3.word_count)
 
-test = StonkRedditAPI('r/wallstreetbets', 'hot', 'all', 100)
-print('Results', test.word_count)
+yahoo_test = YahooFinanceSearch( 'reddit', test1, test2, test3,)
